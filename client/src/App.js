56e7c99ec32340edd50c86a94d97ccf0053b10c8
darielguerra <darielguerra1@gmail.com>
2024-03-18 19:32:39 -0400

    Added modern-normalize CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Axios from "axios";
-import "./App.css";
+import "./styles/App.css";
 
 /* Todo Object:   
                  description: string
